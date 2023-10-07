c04a94e2ac7efd9c537ae46d262c59bfd3b0ae76
JRonaldo79 <jronaldoma@gmail.com>
2023-10-06 20:34:48 -0600

    Se agrego el codigo para comprobar si una palabra es palíndroma

--- Palabra_Palidroma.py
+++ Palabra_Palidroma.py
@@ -1,1 +1,28 @@
 
+# Funcion para comprobar si la palabra es palíndroma
+def es_palindroma(palabra):
+
+    # Elimina espacios en blanco y convierte la palabra a minusculas
+    palabra = palabra.replace(" ", " ").lower()
+
+    # Compruba si la palabra es igual a su inversa
+    if palabra == palabra[::-1]:
+        return True
+    else:
+        return False
+
+# Funcion para inrgresar una palabra y verificar si es o no palíndroma
+
+
+def main():
+    palabra = input("Ingrese una palabra: ")
+
+    if es_palindroma(palabra):
+        print(f"'{palabra}' es una palabra palíndroma. ")
+
+    else:
+        print(f"'{palabra}' no es una palabra palíndroma. ")
+
+
+if __name__ == "__main__":
+    main()
